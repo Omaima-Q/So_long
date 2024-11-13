0a4081c4ef72e2611bb49330f0dfb29f55089c96
Omaima-Q <oqaroot@student.42abudhabi.ae>
2024-11-13 15:05:08 +0400

    Add files via upload

---
+++ srcs/bonus/end_game_options.c
@@ -0,0 +1,89 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   end_game_options.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: omaimaqaroot <omaimaqaroot@student.42.f    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/12 16:46:16 by omaimaqaroo       #+#    #+#             */
+/*   Updated: 2024/11/13 14:11:03 by omaimaqaroo      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include  "../includes/so_long.h"
+
+/*Destroys all the images, clears and destroy the mlx window and frees the map 
+before writing "GAME ENDED" to the terminal and exiting the game.*/
+int	end_game(t_game *game)
+{
+	destroy_images(game);
+	mlx_clear_window(game->mlx, game->window);
+	mlx_destroy_window(game->mlx, game->window);
+	free_map(game->map, game);
+	ft_putstr_fd("GAME ENDED\n", 1);
+	exit(errno);
+	return (0);
+}
+
+/*Destroys all the images, clears the mlx window, frees the map and starts a new
+game.*/
+// void	reset_game(t_game *game)
+// {
+// 	destroy_images(game);
+// 	mlx_clear_window(game->mlx, game->window);
+// 	mlx_destroy_window(game->mlx, game->window);
+// 	free_map(game->map, game);
+// 	start_game(game);
+// }
+
+/*Moves the arrow to the position specified by the direction argument.*/
+// void	put_arrows(t_game *game, int direction)
+// {
+// 	int	x;
+// 	int	y;
+
+// 	x = game->lines / 2;
+// 	y = game->columns / 2;
+// 	put_image(game, game->sprite.start_sign, x, y);
+// 	put_image(game, game->sprite.exit_sign, x + 1, y);
+// 	if (direction == UP)
+// 	{
+// 		put_image(game, game->sprite.base, x + 1, y - 1);
+// 		if (game->map[x + 1][y - 1] == WALL)
+// 			put_image(game, game->sprite.wall, x + 1, y - 1);
+// 		put_image(game, game->sprite.arrow, x, y - 1);
+// 		game->arrow_position = UP;
+// 	}
+// 	else if (direction == DOWN)
+// 	{
+// 		put_image(game, game->sprite.base, x, y - 1);
+// 		if (game->map[x][y - 1] == WALL)
+// 			put_image(game, game->sprite.wall, x, y - 1);
+// 		put_image(game, game->sprite.arrow, x + 1, y - 1);
+// 		game->arrow_position = DOWN;
+// 	}
+// }
+
+/*If the game->arrow_position is UP, resets the game. If it is DOWN, the
+game ends.*/
+// void	select_option(t_game *game)
+// {
+// 	if (game->arrow_position == UP)
+// 		reset_game(game);
+// 	else if (game->arrow_position == DOWN)
+// 		end_game(game);
+// }
+
+/*Linked to the mlx_key_hook function, so each time the allowed key is
+pressed, it activates the associated function to either move the arrow
+up or down, or select an option.*/
+// int	restart_game(int key, void *game)
+// {
+// 	if (key == DOWN || key == 1)
+// 		put_arrows(game, DOWN);
+// 	if (key == UP || key == 13)
+// 		put_arrows(game, UP);
+// 	if (key == ENTER)
+// 		select_option(game);
+// 	return (0);
+// }
